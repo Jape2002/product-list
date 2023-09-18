46cfac78544c0b0e03e643b3eaebba0a5ccd35d0
Jape2002 <124477379+Jape2002@users.noreply.github.com>
2023-09-18 09:07:13 +0800

    view

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,14 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+    public function jape()
+    {
+        return view('products');
+    }
     public function index()
     {
         //
